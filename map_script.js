b33c6be00ddcbf63b15b258f8afe655b0795ff78
Dad <twisterfish.ai@gmail.com>
2024-02-14 15:52:56 -0600

    more changes

--- map_script.js
+++ map_script.js
@@ -1,4 +1,12 @@
 function allowDrop(ev) {
+  var t = ev.target;
+  while (t !== null && !t.classList.contains("grid-item")) {
+    t = t.parentNode;
+  }
+  if (t && t.childNodes.length > 0) {
+    return false;
+  }
+
   ev.preventDefault();
 }
 
@@ -11,26 +19,25 @@ function drop(ev) {
   var data = ev.dataTransfer.getData("text");
   ev.target.appendChild(document.getElementById(data));
   // let animations = document.querySelector(".grid-item").getAnimations();
-
-  console.log(data);
-  console.log(ev.target);
-  console.log(animations);
+  //console.log(data);
+  //console.log(ev.target);
+  //console.log(animations);
 }
 
-window.onload = function() {
+window.onload = function () {
   var gridContainer = document.querySelector('.grid-container');
   gridContainer.innerWidth = screen.availWidth;
   var numColumns = Math.floor(gridContainer.innerWidth / 100);
   gridContainer.style.gridTemplateColumns = `repeat(${numColumns}, 1fr)`;
-  
+
   var numRows = Math.floor(gridContainer.innerHeight / 100);
-   gridContainer.style.gridTemplateRows = `repeat(${numRows}, 100px)`;
+  gridContainer.style.gridTemplateRows = `repeat(${numRows}, 100px)`;
 }
 
-window.onresize = function(){
+window.onresize = function () {
   var gridContainer = document.querySelector('.grid-container');
   gridContainer.innerWidth = screen.availWidth;
-  var numColumns = Math.floor(gridContainer.innerWidth / 100);  
+  var numColumns = Math.floor(gridContainer.innerWidth / 100);
   gridContainer.style.gridTemplateColumns = `repeat(${numColumns}, 1fr)`;
 
   /*var numRows = Math.floor(gridContainer.innerHeight / 100);*/
