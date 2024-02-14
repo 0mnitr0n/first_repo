3f10c6563e907b3beb08f310fd19e73c73d6f5e0
Dad <twisterfish.ai@gmail.com>
2024-02-13 21:22:26 -0600

    more changes

--- map_script.js
+++ map_script.js
@@ -10,23 +10,26 @@ function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
   ev.target.appendChild(document.getElementById(data));
+  // let animations = document.querySelector(".grid-item").getAnimations();
+
   console.log(data);
   console.log(ev.target);
+  console.log(animations);
 }
 
 window.onload = function() {
   var gridContainer = document.querySelector('.grid-container');
-  gridContainer.innerWidth = window.innerWidth;
+  gridContainer.innerWidth = screen.availWidth;
   var numColumns = Math.floor(gridContainer.innerWidth / 100);
   gridContainer.style.gridTemplateColumns = `repeat(${numColumns}, 1fr)`;
   
   var numRows = Math.floor(gridContainer.innerHeight / 100);
-  gridContainer.style.gridTemplateRows = `repeat(${numRows}, 100px)`;
+   gridContainer.style.gridTemplateRows = `repeat(${numRows}, 100px)`;
 }
 
 window.onresize = function(){
   var gridContainer = document.querySelector('.grid-container');
-  gridContainer.innerWidth = window.innerWidth;
+  gridContainer.innerWidth = screen.availWidth;
   var numColumns = Math.floor(gridContainer.innerWidth / 100);  
   gridContainer.style.gridTemplateColumns = `repeat(${numColumns}, 1fr)`;
 
